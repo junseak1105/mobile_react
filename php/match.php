1dd0ba728b84d9e7bbb2938895c3e55a5051b94a
김재훈 <junseak11051@gmail.com>
2022-05-21 17:48:57 +0900

    매칭작업완료, 매칭시 상대 동의 팝업창 완료

--- php/match.php
+++ php/match.php
@@ -20,31 +20,33 @@
     mysqli_query($conn, "set session character_set_results=utf8;");
     mysqli_query($conn, "set session character_set_client=utf8;");
 
-    $userID = 'test';
-    $favor_category = 'food';
-    $select_sex = 'M';
-    $select_list = 'JPN,CHN';
-    $select_time = 'mon1';
+    $userID = $_GET['userID'];
+    $select_time = $_GET['selected_time'];
+    $select_sex = $_GET['selected_sex'];
+    $select_list = $_GET['selected_food'].','.$_GET['selected_hobby'];
+    
     $sqlwhere = "";
     $select_list_arr = explode(',',$select_list);
     $select_list_count = count($select_list_arr);
     $match_idx = ""; //매칭된 idx 저장용
 
     //매칭 신청 내용 등록
-    $query = "insert into match_$favor_category(userID,select_time,select_favor_sex,select_favor_list) values('$userID','$select_time','$select_sex','$select_list')"; 
+    $query = "INSERT INTO match_table(userID,select_time,select_favor_sex,select_favor_list)
+    select '$userID','$select_time','$select_sex','$select_list' from dual 
+    WHERE NOT EXISTS(SELECT userID,select_time FROM match_table WHERE userID='$userID' and select_time = '$select_time')";
     $statement = mysqli_prepare($conn, $query);
     mysqli_stmt_execute($statement);
     
     //매칭 일치 사례 판별
     $sqlwhere = "where select_favor_list = '$select_list' and userID != '$userID'";
-    $query = "SELECT (IF(EXISTS(SELECT * FROM match_$favor_category $sqlwhere),'true', 'false')) as result";
+    $query = "SELECT (IF(EXISTS(SELECT * FROM match_table $sqlwhere),'true', 'false')) as result";
 	$data = $conn->query($query)->fetch_array();
     
     if($data['result'] == 'true'){ //정확한 매치
-        $query = "select idx from match_$favor_category $sqlwhere limit 1";
+        $query = "select idx from match_table $sqlwhere limit 1";
         $data = $conn->query($query)->fetch_array();
     }else{ //차선책(선택사항 중 하나라도 맞을 시)
-        $sqlwhere = "where ( ";
+        $sqlwhere = "where ( select_favor_sex = '$select_sex' and";
         for($i=0;$i<$select_list_count;$i++){
             $sqlwhere = $sqlwhere."select_favor_list like '%".$select_list_arr[$i]."%' "; //select_list explode()된 값 like 조건문 삽입
             if($i!=($select_list_count-1)){
@@ -54,12 +56,13 @@
             }
         }
         $sqlwhere = $sqlwhere." and userID != '$userID'";
-        $query = "select idx from match_$favor_category $sqlwhere limit 1"; //최상단 1명 검색(랜덤x)
+        $query = "select idx,userID,select_favor_list from match_table $sqlwhere ORDER BY RAND() LIMIT 0, 1"; //최상단 1명 검색(랜덤x)
         echo $query;
         $data = $conn->query($query)->fetch_array();
     }
     
     mysqli_close($conn);
-    echo json_encode($data);
+    $responseJson = json_encode(array('results' => $data),JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+    echo $responseJson;
     
 ?>
--- src/MatchScreen.js
+++ src/MatchScreen.js
@@ -1,85 +1,189 @@
-import React, { useState,useEffect } from "react";
-import {Button,ActivityIndicator, FlatList, SafeAreaView, TextInput, isSelectedBar, StyleSheet, Text,TouchableOpacity,View,ScrollView } from "react-native";
+import React, {useState, useEffect} from 'react';
+import {
+  Button,
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  TextInput,
+  isSelectedBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  Modal,
+  Pressable,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
-import MyButton from "../components/MyButton";
+import MyButton from '../components/MyButton';
 import CheckboxList from 'rn-checkbox-list';
 
-
-
-
 const MatchScreen = () => {
-
-  const [selectedFood,setselectedFood] = useState([]);
-  const [selectedHobby,setselectedHobby] = useState([]);
-
-  //db접속
+  //팝업창
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modaltext,setmodaltext] = useState();
+  const [modalinfo,setmodalinfo] = useState();
+  //match 성공 여부 저장
+  const [match_result, setmatchresult] = useState([]);
+  //카테고리 받아오기 시작
+  //선택된 값 저장
+  const [selectedFood, setselectedFood] = useState([]);
+  const [selectedHobby, setselectedHobby] = useState([]);
+  const [selectedSex, setselectedSex] = useState([]);
+  //카테고리 받아온 값 저장
   const [isLoading, setLoading] = useState(true);
   const [fa_hobby, sethobbyData] = useState([]);
   const [fa_food, setfoodData] = useState([]);
-
+  const [fa_sex, setsexData] = useState([]);
+  //db접속
   const getcategory = async () => {
-     try {
-      const response_food = await fetch('http://jhk.n-e.kr:8080/test.php?table=favor_ca&co_code=fa_food'); //1 CURL로 연결(php)
-      const response_hobby = await fetch('http://jhk.n-e.kr:8080/test.php?table=favor_ca&co_code=fa_hobby');
+    try {
+      const response_food = await fetch(
+        'http://jhk.n-e.kr:80/get_category.php?table=favor_ca&co_code=fa_food',
+      ); //1 CURL로 연결(php)
+      const response_hobby = await fetch(
+        'http://jhk.n-e.kr:80/get_category.php?table=favor_ca&co_code=fa_hobby',
+      );
+      const response_sex = await fetch(
+        'http://jhk.n-e.kr:80/get_category.php?table=favor_ca&co_code=fa_sex',
+      );
       const json_food = await response_food.json(); //2 json 받아온거 저장
       const json_hobby = await response_hobby.json();
+      const json_sex = await response_sex.json();
       sethobbyData(json_hobby.results); //3 const배열에다가 저장
       setfoodData(json_food.results);
-      
+      setsexData(json_sex.results);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
-  }
+  };
   //db접속끝
-  
+
   useEffect(() => {
     getcategory(); //받아오는 함수 실행
   }, []);
+  //카테고리 받아오기 끝
 
+  //매칭 버튼 클릭 시
+  const set_match = async () => {
+    let selected_sex = selectedSex.toString();
+    let selected_food = selectedFood.toString();
+    let selected_hobby = selectedHobby.toString();
+    let userID = 'test';
+    let selected_time = 'mon1';
 
+    try {
+      const response_match = await fetch(
+        'http://jhk.n-e.kr:80/match.php?userID='+userID +'&selected_time='+selected_time +'&selected_sex=' +selected_sex +'&selected_food='+selected_food+'&selected_hobby='+selected_hobby
+      ); //1 CURL로 연결(phselected_foodp)
+      const json_match = await response_match.json(); //2 json 받아온거 저장
+      setmatchresult(json_match.results); //3 const배열에다가 저장
+      match_complete(match_result);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  //팝업창 컴포넌트
+  const Modal_view = props => {
+    if(match_result.count == 0){
+      setmodaltext("매칭가능 대상이 없습니다. 대기합니다");
+    }else{
+      setmodaltext("매칭성공 이분과 만나보시겠습니까?");
+      setmodalinfo("닉네임"+match_result.userID+"체크된 문항"+match_result.select_favor_list);
+    }
+  
+    return (
+      <View style={{backgroundColor:"grey"}}>
+      <Text>{modaltext}</Text>
+      <Text>{modalinfo}</Text>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setModalVisible(!modalVisible)}
+      >
+        <Text style={styles.textStyle}>매칭 동의</Text>
+      </Pressable>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setModalVisible(!modalVisible)}
+      >
+        <Text style={styles.textStyle}>취소</Text>
+      </Pressable>
+      </View>
+    );
+  };
+
+  //매칭 완료 후 팝업창
+  const match_complete = (results) =>{
+    setModalVisible(true);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.idx}, {item.userName}</Text>
-          )}
-        />
-      )}
-    </View> */}
       <View style={styles.container}>
         <View style={styles.container_sex}>
-          <MyButton/>
-          <MyButton/>
-          <MyButton/>
+          <CheckboxList
+            headerName="전체"
+            style={styles.chkbox_sex}
+            listItems={fa_sex}
+            selectedListItems={selectedSex}
+            listItemStyle={{borderBottomColor: 'black', borderBottomWidth: 1}}
+            onChange={({ids, items}) => setselectedSex(ids)}
+          />
         </View>
-        <View style = {styles.selector_hobby_food}>
+        <View style={styles.selector_hobby_food}>
           <SafeAreaView style={styles.container_hobby}>
-            <CheckboxList 
-              headerName="전체" 
-              listItems={fa_hobby} 
-              selectedListItems={selectedHobby} 
-              onChange={({ ids, items }) => console.log('My updated list :: ', ids)}
+            <CheckboxList
+              headerName="전체"
+              listItems={fa_hobby}
+              selectedListItems={selectedHobby}
+              listItemStyle={{borderBottomColor: 'black', borderBottomWidth: 1}}
+              onChange={({ids, items}) => setselectedHobby(ids)}
             />
           </SafeAreaView>
-          <SafeAreaView style={styles.container_food}>   
-            <CheckboxList 
-              headerName="전체" 
-              listItems={fa_food} 
-              selectedListItems={selectedFood} 
-              onChange={({ ids, items }) => console.log('My updated list :: ', ids)}
+          <SafeAreaView style={styles.container_food}>
+            <CheckboxList
+              headerName="전체"
+              listItems={fa_food}
+              selectedListItems={selectedFood}
+              listItemStyle={{borderBottomColor: 'black', borderBottomWidth: 1}}
+              onChange={({ids, items}) => setselectedFood(ids)}
             />
           </SafeAreaView>
         </View>
         <View style={styles.set_match}>
-          <MyButton/>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#4B778D',
+              padding: 16,
+              margin: 10,
+              borderRadius: 8,
+            }}
+            onPress={() => set_match()}>
+            <Text style={{fontSize: 10, color: 'white'}}>My Button</Text>
+          </TouchableOpacity>
         </View>
+      </View>
+      {/* 팝업창 */}
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <Modal_view
+            
+            />
+          </View>
+        </Modal>
       </View>
     </SafeAreaView>
   );
@@ -89,39 +193,62 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  hobby_checkbox: {
-    flexDirection:'row',
-  },
-  item_food: {
-    width:20,
-    height:20,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 8,
-  },
-  title: {
-    fontSize: 32,
+  container_sex: {
+    flexDirection: 'row',
   },
   selector_hobby_food: {
     margin: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    flex:6,
+    flex: 6,
   },
-  container_food:{
-    flex:1,
+  container_food: {
+    flex: 1,
   },
-  container_hobby:{
-    flex:1,
+  container_hobby: {
+    flex: 1,
   },
-  container_sex:{
-    flexDirection:'row',
+  set_match: {
+    width: 100,
+    flex: 1,
   },
-  set_match:{
-    width:100,
-    flex:1,
-  }
+  //modal css start
+  centeredView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  //modal css end
 });
-
 
 export default MatchScreen;
