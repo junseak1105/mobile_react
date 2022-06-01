789bf19271708d3eb51d09ac8d5f12cd36edff13
김재훈 <junseak11051@gmail.com>
2022-06-02 00:32:47 +0900

    매칭용 php 개선(경우의 수 추가)

--- php/set_match.php
+++ php/set_match.php
@@ -7,13 +7,22 @@
 
     $matched_param = $_GET['matched'];
     $match_userid = $_GET['match_userid'];
-    $userID = isset($_GET['userID']) ? $_GET['page'] : "";
+    $userID = $_GET['userID'];
     $selected_hour = $_GET['selected_hour'];
     $selected_day = $_GET['selected_day'];
     $select_time = $selected_day.$selected_hour;
     
-    $sql = 'call matched('$matched_param','$selected_day','$selected_hour','$userID','$match_userid','$select_time')';
-    mysqli_query($conn,$sql);
+    if($matched_param == "nomatch"){ //매치 없을 시 
+        $sql = "update user_timetable set $selected_day='Wait,1' where userID = '$userID' and hour = '$selected_hour';"; //단순 대기
+    }else if($matched_param == "matched"){//매치 있을 시, 신청/수락대기 상태로 변경
+        $sql = "update user_timetable set $selected_day='Pend,2' where (userID = '$userID' or userID = '$match_userid') and hour = '$selected_hour';"; //유저 테이블 매칭수락or수락대기로 설정
+        mysqli_query($conn,$sql);
+        mysqli_close($conn);
+        include("db.php");
+        $sql = "update match_table set pending='Y' where (userID = '$userID' or userID = '$match_userid') and select_time = '$select_time';"; //매칭용테이블 pending y로 설정: 중복 매칭 방지
+        mysqli_query($conn,$sql);
+        mysqli_close($conn);
+    }
     echo json_encode(array('results' => $arr),JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
 
     
