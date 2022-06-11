e9bfe8e1c95c4810e2edc1298d27ad4190781745
do-bright <godqhrgotjs@gmail.com>
2022-06-12 03:31:15 +0900

    chat 환경설정 충돌해결 / realtimedb 생성후 연결중

--- App.js
+++ App.js
@@ -17,12 +17,30 @@ import ChatScreen from './src/ChatScreen';
 // create a "stack"
 const MyStack = createNativeStackNavigator();
 
+// // firebase
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBVDkSExWDgCvrTGJoGElTUPn_tNfL-nX4',
+  authDomain: 'mobileprogram-e7cbf.firebaseapp.com',
+  databaseURL: 'https://mobileprogram-e7cbf-default-rtdb.firebaseio.com',
+  projectId: 'mobileprogram-e7cbf',
+  storageBucket: 'mobileprogram-e7cbf.appspot.com',
+  messagingSenderId: '224863518305',
+  appId: '1:224863518305:web:ebd71921577c17cd837420',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const App = () => {
   return (
     <NavigationContainer>
-      <MyStack.Navigator
-        screenOptions={{ headerShown: false }}
-      >
+      <MyStack.Navigator screenOptions={{headerShown: false}}>
         <MyStack.Screen name="MainScreen" component={MainScreen} />
         <MyStack.Screen name="MatchScreen" component={MatchScreen} />
         <MyStack.Screen name="LoginScreen" component={LoginScreen} />
--- src/ChatScreenOriginal.js
+++ src/ChatScreenOriginal.js
@@ -1,56 +1,53 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  Image,
-  // CheckBox,
-} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
-import {
-  collection,
-  addDoc,
-  orderBy,
-  query,
-  onSnapshot,
-} from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getCharsetNumber} from 'mysql/lib/ConnectionConfig';
-// import {getDatabase, get, ref, onVlaue} from 'react-native-firebase/database';
 
 const ChatScreen = ({route, navigation}) => {
   const [messages, setMessages] = useState([]);
+  const [customText, setcustomText] = useState();
 
-  // const [customText, setcustomText] = useState();
-
-  // //navigation data
+  //navigation data
   const token = route.params.token;
   const user1_id = route.params.user1_id;
   const user2_id = route.params.user2_id;
   const param_hour = route.params.param_hour;
   const param_day = route.params.param_day;
+  //results
+  const [returnMsg, setreturnMsg] = useState([]);
 
   useEffect(() => {
     chat_fetch();
-    const collectionRef = collection(database, 'chats');
-    const q = query(collectionRef, orderBy('createdAt', 'desc'));
-
-    const unsubscribe = onSnapshot(q, querySnapshot => {
-      setMessages(
-        querySnapshot.docs.map(doc => ({
-          _id: doc.data()._id,
-
-          createdAt: doc.data().createdAt.toDate(),
-          text: doc.data().text,
-          user: doc.data().user,
-        })),
-      );
-    });
-
-    return () => unsubscribe();
   }, []);
 
+  const chat_input = async () => {
+    let now = new Date();
+    try {
+      const response_table = await fetch(
+        'http://jhk.n-e.kr:80/chat_input.php?userID=' +
+          token +
+          '&user1_id=' +
+          user1_id +
+          '&user2_id=' +
+          user2_id +
+          '&text=' +
+          customText +
+          '&createdAt=' +
+          now.toISOString() +
+          '&selected_hour=' +
+          param_hour +
+          '&selected_day=' +
+          param_day,
+      ); //1 CURL로 연결(php)
+      const json_table = await response_table.json(); //2 json 받아온거 저장
+      setreturnMsg(json_table.results); //3 const배열에다가 저장
+      chat_fetch();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // setLoading(false);
+    }
+  };
   const chat_fetch = async () => {
     console.log(
       'http://jhk.n-e.kr:80/chat_fetch.php?userID=' +
@@ -87,33 +84,23 @@ const ChatScreen = ({route, navigation}) => {
       // setLoading(false);
     }
   };
-
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    );
-    const {_id, createdAt, text, user} = messages[0];
-    addDoc(collection(database, 'chats'), {
-      _id,
-      createdAt,
-      text,
-      user,
-    });
-    console.log(messages);
-  }, []);
+  //   const onSend = useCallback((messages = []) => {
+  //     setMessages(previousMessages =>
+  //       GiftedChat.append(previousMessages, messages),
+  //     );
+  //     console.log(messages);
+  //   }, []);
 
   return (
-    <>
-      <GiftedChat
-        messages={messages}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: token,
-          avatar: 'https://placeimg.com/140/140/any',
-        }}
-      />
-    </>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => chat_input()}
+      user={{
+        _id: token, //유저 이름으로 구별
+      }}
+      text={customText}
+      onInputTextChanged={text => setcustomText(text)}
+    />
   );
-  const styles = StyleSheet.create({});
 };
 export default ChatScreen;
